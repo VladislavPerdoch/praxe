06b0ed2fff82d8c6e49e3244fd826559b4d26555
Vladislav Perdoch <v.perdoch.st@spseiostrava.cz>
2023-05-26 15:06:43 +0200

    http_crash_course

--- http_crash_course/test.js
+++ http_crash_course/test.js
@@ -1,5 +1,5 @@
-const express = require("express");
-const path = require("path");
+const express = require('express');
+const path = require('path');
 
 
 const app = express();
@@ -7,8 +7,39 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false}));
 
-app.get("/", (req, res) => {
-    res.send("Hello from Express");
+/*app.post('/contact', (req, res) => {
+    if(!req.body.name) {
+        return res.status(400).send("Name is required");
+    }
+
+
+    res.status(201).send("Thank you " + req.body.name);
 });
 
-app.listen(5000, () => console.log("Server started on 5000"));
+app.post("/login", (req, res) => {
+    if(!req.header("x-auth-token")) {
+        return res.status(400).send("No Token");
+    }
+
+    if(req.header("x-auth-token") !== "123456") {
+        return res.status(401).send("Not authorized");
+    }
+    res.send("Legged in");
+})*/
+
+app.put("/post/:id", (req, res) => {
+
+    res.json({
+        id: req.params.id,
+        title: req.body.title
+    })
+
+})
+
+
+
+app.listen(5500, () => console.log('Server started on 5500'));
+
+
+//res.send(req.body.name/email);
+//req.header/body
