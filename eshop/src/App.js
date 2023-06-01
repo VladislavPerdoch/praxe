94d7dffb643ffb0dacc55edf8f7982c8dd87ca17
Vladislav Perdoch <v.perdoch.st@spseiostrava.cz>
2023-06-01 15:29:10 +0200

    finishers

--- eshop/src/App.js
+++ eshop/src/App.js
@@ -8,6 +8,9 @@ import {Products, Navbar, Cart, Checkout} from "./components";
 const App = () => {
     const [products, setProducts] = useState([]);
     const [cart, setCart] = useState({});
+    const [order, setOrder] = useState({});
+    const [errorMessage, setErrorMessage] = useState('');
+    const [mobileOpen, setMobileOpen] = React.useState(false);
 
     const handleAddToCart = async (productId, quantity) => {
         await commerce.cart.add(productId, quantity);
@@ -49,6 +52,28 @@ const App = () => {
         setCart(cart);
     }
 
+    const refreshCart = async () => {
+        const newCart = await commerce.cart.refresh();
+    
+        setCart(newCart);
+      };
+    
+    
+    
+    const handleCaptureCheckout = async (checkoutTokenId, newOrder) => {
+        try {
+          const incomingOrder = await commerce.checkout.capture(checkoutTokenId, newOrder);
+    
+          setOrder(incomingOrder);
+    
+          refreshCart();
+        } catch (error) {
+          setErrorMessage(error.data.error.message);
+        }
+      };
+
+
+
     return (
         <Router>
             <div>
@@ -64,7 +89,7 @@ const App = () => {
                             onUpdateCartQty={handleUpdateCartQty} 
                             onRemoveFromCart={handleRemoveFromCart} 
                             onEmptyCart={handleEmptyCart}/> } />
-                    <Route exact path="/checkout" element={<Checkout cart={cart}/>}/>
+                    <Route exact path="/checkout" element={<Checkout cart={cart} order={order} onCaptureCheckout={handleCaptureCheckout} error={errorMessage}/>}/>
                     
                 </Routes>
             </div>
